32a7bf5867f5035cc0aa9ef5eef27190809b4675
PandaDeveloper13 <9025264@student.zadkine.nl>
2025-12-16 08:47:27 +0100

    added database

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/login', function () {
     return view('login');
 });
